b3169c3b51fb37fc75f839c0e4090e09b290f615
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2024-04-29 02:51:09 +0200

    Fix linting

--- pypricer/api.py
+++ pypricer/api.py
@@ -6,11 +6,6 @@ from pypricer.simulations import wiener_process
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Go to /docs for the OpenAPI"}
-
-
 @app.get("/price/european_call", tags=["Bachelier model"])
 async def price_european_call(
     strike_price: float,
@@ -22,6 +17,21 @@ async def price_european_call(
     mu: float,
     rfr: float,
 ):
+    """Calculate the price of a European call option using the Bachelier model.
+
+    Args:
+        strike_price (float): The strike price of the option.
+        expiry (float): Time to expiration in years.
+        n_paths (int): Number of simulated asset price paths.
+        n_steps (int): Number of time steps in each path.
+        dt (float): Time increment for each step in the simulation.
+        sigma (float): Volatility of the asset price.
+        mu (float): Drift of the asset price process.
+        rfr (float): Risk-free rate.
+
+    Returns:
+        dict: A dictionary containing the calculated option price.
+    """
     option = european.EuropeanCallOption(strike_price=strike_price, expiry=expiry)
     paths = wiener_process.generate_wiener_process(
         n_paths=n_paths, n_steps=n_steps, dt=dt, sigma=sigma, mu=mu
--- pypricer/options/european.py
+++ pypricer/options/european.py
@@ -1,21 +1,55 @@
-from enum import Enum
+"""
+Option Pricing Module
+
+This module provides classes for pricing European options using different protocols.
+"""
+
 import numpy as np
+import pandas as pd
 
 
 class EuropeanOption:
+    """Parent class for european options"""
+
     def __init__(self, strike_price: float, expiry: float):
         self.strike_price = strike_price
         self.expiry = expiry
 
-    def payoff(self, path: np.ndarray) -> np.ndarray:
-        raise NotImplementedError(f"Payoff needs to be implemented in child classes")
+    def payoff(self, path: pd.DataFrame) -> pd.DataFrame:
+        """Abstract method"""
+        raise NotImplementedError("Payoff needs to be implemented in child classes")
+
+    def price(self, path: pd.DataFrame, rfr: float) -> float:
+        """Abstract method"""
+        raise NotImplementedError("Pricing needs to be implemented in child classes")
 
 
 class EuropeanCallOption(EuropeanOption):
-    def payoff(self, path: np.ndarray) -> np.ndarray:
+    """Child class implementing call option protocol"""
+
+    def payoff(self, path: pd.DataFrame) -> np.ndarray:
+        """Compute option payoff
+
+        Args:
+            path (pd.DataFrame): Dataset with asset price realizations.
+                Columns are trajectories, rows is the time index.
+
+        Returns:
+            pd.DataFrame: Dataset with payoffs for each trajectory
+        """
         return np.maximum(0.0, path.iloc[-1, :] - self.strike_price)
 
-    def price(self, path: np.ndarray, rfr: float) -> np.ndarray:
+    def price(self, path: pd.DataFrame, rfr: float) -> np.ndarray:
+        """Compute the price of the option using the Black-Scholes formula.
+
+        Args:
+            path (pd.DataFrame): DataFrame with asset price realizations.
+                Columns represent different trajectories, rows represent time steps.
+            rfr (float): Risk-free rate.
+
+        Returns:
+            np.ndarray: Array containing option prices for each trajectory.
+        """
         return np.exp(-self.expiry * rfr) * self.payoff(path=path).mean()
 
 
